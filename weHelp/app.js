193b22fd7063932189f9ba45a8ad3d0a4be9120d
youcen-wu <153118119+youcen-wu@users.noreply.github.com>
2024-03-01 22:59:03 +0800

    Update app.js

--- weHelp/app.js
+++ weHelp/app.js
@@ -1,4 +1,4 @@
 const button = document.querySelector("#button");
 button.addEventListener("click", function () {
-  window.location.href = "https://mellifluous-eclair-80c686.netlify.app/";
+  window.location.href = "https://65e1e554d0709be4c036ed87--magnificent-meerkat-165435.netlify.app/";
 });
